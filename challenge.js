fde75c4ada2313203a1d099396ad391e8ab08cba
= <=>
2023-10-02 19:43:49 +0100

    third commit (if-else-chained and switch statement)

--- challenge.js
+++ challenge.js
@@ -1,9 +1,39 @@
-for (let i = 0; i < 10; i++) {
-    console.log("Mufti")
-}
+// for (let i = 0; i < 10; i++) {
+//     console.log("Mufti")
+// }
 
-let i = 0;
-    while (i < 10) {
-        console.log("Mohammed")
-        i++;
-    }
+// let i = 0;
+//     while (i < 10) {
+//         console.log("Mohammed")
+//         i++
+//     }
+// // var let const
+
+// let today = "Monday"
+// if (today == "Monday"){
+//     console.log("Its My coding day")
+// } else if (today == "Tuesday"){
+//     console.log("Today is my off day")
+// } else {
+//     console.log("ask me personally which day")
+// }
+let today = "Friday"
+switch (today) {
+    case "Monday":
+        console.log("Its My coding day")
+        break;
+    case "Tuesday":
+        console.log("Today is my off day")
+        break;
+    case "Wednesday":
+        console.log("Today is my gym day")
+        break;
+    case "Thursday":
+        console.log("Today is my school day")
+        break;
+    case "Friday":
+        console.log("Today is my rest day ")
+        break;
+    default:
+        break;
+}
