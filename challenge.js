5d51d4d98640ff7214551ea56199529c8910b9b2
= <=>
2023-10-09 19:29:07 +0100

    added comment for each code

--- challenge.js
+++ challenge.js
@@ -1,13 +1,19 @@
+// // write a for loop that prints your name 10 times
+
 // for (let i = 0; i < 10; i++) {
 //     console.log("Mufti")
 // }
 
+// // write a while loop that prints your name 20 times
+
 // let i = 0;
-//     while (i < 10) {
+//     while (i < 20) {
 //         console.log("Mohammed")
 //         i++
 //     }
 // // var let const
+
+// // write if/else if, chained code to check what day it is
 
 // let today = "Monday"
 // if (today == "Monday"){
@@ -17,23 +23,26 @@
 // } else {
 //     console.log("ask me personally which day")
 // }
-let today = "Friday"
-switch (today) {
-    case "Monday":
-        console.log("Its My coding day")
-        break;
-    case "Tuesday":
-        console.log("Today is my off day")
-        break;
-    case "Wednesday":
-        console.log("Today is my gym day")
-        break;
-    case "Thursday":
-        console.log("Today is my school day")
-        break;
-    case "Friday":
-        console.log("Today is my rest day ")
-        break;
-    default:
-        break;
-}
+
+// // convert the above code to switch from if/else if
+
+// let today = "Friday"
+// switch (today) {
+//     case "Monday":
+//         console.log("Its My coding day")
+//         break;
+//     case "Tuesday":
+//         console.log("Today is my off day")
+//         break;
+//     case "Wednesday":
+//         console.log("Today is my gym day")
+//         break;
+//     case "Thursday":
+//         console.log("Today is my school day")
+//         break;
+//     case "Friday":
+//         console.log("Today is my rest day ")
+//         break;
+//     default:
+//         break;
+// }
